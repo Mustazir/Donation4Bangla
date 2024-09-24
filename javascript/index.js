f519f44911861e72ff039805d6d312d2846736d4
Mustazir Billah <billah15-5193@diu.edu.bd>
2024-09-25 02:52:36 +0600

    All Function work

--- javascript/index.js
+++ javascript/index.js
@@ -11,15 +11,18 @@ function calculate(price, money,parent) {
         price.innerText=parseInt(money)+temp;
         const myAccount=document.querySelector('#total-Account');
         myAccount.innerText=myAccount.innerText-money;
+        const modal=document.querySelector('#modal')
+        modal.classList.remove('hidden')
         const output=document.querySelector('#output')
         const date=new Date()
 
         const h1=parent.children[0].innerText;
         
+        
         let apent=document.createElement('div')
         apent.innerHTML =`<div class="border-[1px] border-zinc-600 p-5 rounded-2xl">
             <h1 class="text-xl font-bold grotesk "> ${money} Taka is ${h1}</h1> 
-            <p class="text-zinc-400">Date : ${date.toDateString()}</p>
+            <p class=" text-slate-800">Date : ${date.toDateString()}</p>
         </div>`;
         output.prepend(apent)
     }
@@ -86,6 +89,12 @@ document.querySelector('#historyBtn').addEventListener('click',function (){
     
  
  })
+
+ document.querySelector('#modal button').addEventListener('click',function (){
+    const modal=document.querySelector('#modal')
+        modal.classList.add('hidden')
+})
+        
 
 
 
