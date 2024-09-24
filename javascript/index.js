f290c1e95ca3f44cf42abd180b0eba7d1be2ceac
Mustazir Billah <billah15-5193@diu.edu.bd>
2024-09-24 22:27:07 +0600

    append done

--- javascript/index.js
+++ javascript/index.js
@@ -11,9 +11,19 @@ function calculate(price, money,parent) {
         price.innerText=parseInt(money)+temp;
         const myAccount=document.querySelector('#total-Account');
         myAccount.innerText=myAccount.innerText-money;
-        console.log(parent)
+        const output=document.querySelector('#output')
+        const date=new Date()
+
+        const h1=parent.children[0].innerText;
+        
+        let apent=document.createElement('div')
+        apent.innerHTML =`<div class="border-[1px] border-zinc-600 p-5 rounded-2xl">
+            <h1 class="text-xl font-bold grotesk "> ${money} Taka is ${h1}</h1> 
+            <p class="text-zinc-400">Date : ${date.toDateString()}</p>
+        </div>`;
+        output.prepend(apent)
     }
-    else if (string.test(money)) {
+    else if (string.test(money) || money.trim() === "") {
         alert('this is not money')
     }
 
@@ -26,7 +36,8 @@ document.querySelector('#donet-btn').addEventListener('click', function (event) 
     
     const donetion = document.querySelector('#price ')
     const inputField = document.querySelector('#card_text ').value;
-    console.log(inputField)
+    const inputFieldcontainer = document.querySelector('.card-text');
+    
     const parent=event.target.parentElement.parentElement
     calculate(donetion, inputField,parent)
     
@@ -49,6 +60,32 @@ document.querySelector('#donet-btnQ').addEventListener('click', function (event)
     calculate(donetion, inputField,parent)
     
 })
+
+
+document.querySelector('#donationBtn').addEventListener('click',function (){
+    const bg=document.querySelector('#donationBtn')
+    bg.classList.add('bg-green-700')
+    const abg=document.querySelector('#historyBtn')
+    abg.classList.remove('bg-green-700')
+    const container= document.querySelector('.card-containerr')
+   container.classList.remove('hidden')
+   const container2= document.querySelector('.history-containerr')
+   container2.classList.add('hidden')
+})
+
+document.querySelector('#historyBtn').addEventListener('click',function (){
+    const bg=document.querySelector('#donationBtn')
+    bg.classList.remove('bg-green-700')
+    const abg=document.querySelector('#historyBtn')
+    abg.classList.add('bg-green-700')
+
+    const container= document.querySelector('.card-containerr')
+    container.classList.add('hidden')
+    const container2= document.querySelector('.history-containerr')
+    container2.classList.remove('hidden')
+    
+ 
+ })
 
 
 
