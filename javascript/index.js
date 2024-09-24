235c9e70b1b21bd0fd1d9318cc3d0e3fb6960010
Mustazir Billah <billah15-5193@diu.edu.bd>
2024-09-25 02:54:30 +0600

    modal function

--- javascript/index.js
+++ javascript/index.js
@@ -11,18 +11,15 @@ function calculate(price, money,parent) {
         price.innerText=parseInt(money)+temp;
         const myAccount=document.querySelector('#total-Account');
         myAccount.innerText=myAccount.innerText-money;
-        const modal=document.querySelector('#modal')
-        modal.classList.remove('hidden')
         const output=document.querySelector('#output')
         const date=new Date()
 
         const h1=parent.children[0].innerText;
         
-        
         let apent=document.createElement('div')
         apent.innerHTML =`<div class="border-[1px] border-zinc-600 p-5 rounded-2xl">
             <h1 class="text-xl font-bold grotesk "> ${money} Taka is ${h1}</h1> 
-            <p class=" text-slate-800">Date : ${date.toDateString()}</p>
+            <p class="text-zinc-400">Date : ${date.toDateString()}</p>
         </div>`;
         output.prepend(apent)
     }
@@ -40,7 +37,7 @@ document.querySelector('#donet-btn').addEventListener('click', function (event) 
     const donetion = document.querySelector('#price ')
     const inputField = document.querySelector('#card_text ').value;
     const inputFieldcontainer = document.querySelector('.card-text');
-    document.querySelector('#card_text ').value=''
+    
     const parent=event.target.parentElement.parentElement
     calculate(donetion, inputField,parent)
     
@@ -49,7 +46,7 @@ document.querySelector('#donet-btnF').addEventListener('click', function (event)
     
     const donetion = document.querySelector('#priceTwo ')
     const inputField = document.querySelector('#card_textFeni ').value;
-    document.querySelector('#card_textFeni ').value=''
+    console.log(inputField)
     const parent=event.target.parentElement.parentElement
     calculate(donetion, inputField,parent)
     
@@ -58,7 +55,7 @@ document.querySelector('#donet-btnQ').addEventListener('click', function (event)
     
     const donetion = document.querySelector('#priceThree ')
     const inputField = document.querySelector('#card_textQuota ').value;
-    document.querySelector('#card_textQuota ').value=''
+    console.log(inputField)
     const parent=event.target.parentElement.parentElement
     calculate(donetion, inputField,parent)
     
@@ -89,12 +86,6 @@ document.querySelector('#historyBtn').addEventListener('click',function (){
     
  
  })
-
- document.querySelector('#modal button').addEventListener('click',function (){
-    const modal=document.querySelector('#modal')
-        modal.classList.add('hidden')
-})
-        
 
 
 
